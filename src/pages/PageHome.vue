3a68ca0c4741331de3ac276f2533f70b77c00f0c
Junaid Farooq <junaid@evercam.io>
2021-06-05 23:49:57 +0500

    Added qweet below icons.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -36,6 +36,63 @@
       class="divider"
       color="grey-2"
     />
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Junaid Farooq</strong>
+            <span class="text-grey-7">
+              @junaid_farooq
+            </span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+            Aliquam voluptatem voluptatibus sit sunt, iusto repellendus voluptate amet eos numquam nam excepturi ab fuga delectus minima molestias. Ex nobis corporis suscipit!
+          </q-item-label>
+          <div class="qweet-icons row justify-between q-mt-sm">
+            <q-btn
+              color="grey"
+              icon="far fa-comment"
+              size="sm"
+              flat
+              round
+            />
+            <q-btn
+              color="grey"
+              icon="fas fa-retweet"
+              size="sm"
+              flat
+              round
+            />
+            <q-btn
+              color="grey"
+              icon="far fa-heart"
+              size="sm"
+              flat
+              round
+            />
+            <q-btn
+              color="grey"
+              icon="fas fa-trash"
+              size="sm"
+              flat
+              round
+            />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -60,4 +117,10 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-5
+
+.qweet-content
+  white-space: pre-line
+
+.qweet-icons
+  margin-left: -5px
 </style>
